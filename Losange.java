2776db06692860fc6fb051f874b5a80a9ad21b22
nullbyt3n <nullbyt91@gmail.com>
2024-11-18 12:00:01 +0100

    second commit

---
+++ Losange.java
@@ -0,0 +1,13 @@
+public class Losange {
+    private double diagonal1;
+    private double diagonal2;
+
+    public Losange(double diagonal1, double diagonal2) {
+        this.diagonal1 = diagonal1;
+        this.diagonal2 = diagonal2;
+    }
+
+    public double area() {
+        return (diagonal1 * diagonal2) / 2;
+    }aeaeaeaeaeae
+}
